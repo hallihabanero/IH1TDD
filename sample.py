4d7554fe54605c889c740087b015097a403b0f91
hallli <haraldur.sigfus@gmail.com>
2023-02-27 21:35:13 +0000

    taxed orders

--- sample.py
+++ sample.py
@@ -1,7 +1,11 @@
 class orders():
     def __init__(self, price):
         self.price = price
-        self.tax = 0.08
+        self.tax = {
+            "USA": 0.8,
+            "Panama": 0.0,
+            "Iceland": 2.8
+        }
         self.shipping = 0.05
     
     def calculate_single_item_no_tax_nofees(self, item):
@@ -11,4 +15,11 @@ class orders():
         total_cost = 0
         for item in items:
             total_cost += self.calculate_single_item_no_tax_nofees(item)
+        return total_cost
+    
+    def calculate_taxed_order(self, items, location):
+        total_cost = self.calculate_multiple_items_no_tax_no_fees(items)
+        for i in self.tax:
+            if location == i:
+                total_cost += total_cost * self.tax[i]
         return total_cost
--- test_sample.py
+++ test_sample.py
@@ -9,8 +9,16 @@ def test_single_item_no_fees_no_taxes():
     
 def test_multiple_items_no_fees_no_taxes():
     items = [{"Name": "Nocco", "Price": 50, "Weight": 1, "Quantity": 1},
-             {"Name": "Collab", "Price": 40, "Weight": 1, "Quantity": 1}]
-    expected = 90
+             {"Name": "Collab", "Price": 40, "Weight": 0.8, "Quantity": 2}]
+    expected = 130
     order = orders(1)
     actual = order.calculate_multiple_items_no_tax_no_fees(items)
+    assert expected == actual
+
+def test_taxed_order():
+    items = [{"Name": "Nocco", "Price": 50, "Weight": 1, "Quantity": 1},
+             {"Name": "Collab", "Price": 40, "Weight": 0.8, "Quantity": 2}]
+    expected = 234
+    order = orders(1)
+    actual = order.calculate_taxed_order(items, "USA")
     assert expected == actual
