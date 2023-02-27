f58e221d9f4a669b621909798710059ef3ab25de
hallli <haraldur.sigfus@gmail.com>
2023-02-27 21:17:26 +0000

    test mutiple  items

--- sample.py
+++ sample.py
@@ -7,3 +7,8 @@ class orders():
     def calculate_single_item_no_tax_nofees(self, item):
         return item["Price"] * item["Quantity"]
     
+    def calculate_multiple_items_no_tax_no_fees(self, items):
+        total_cost = 0
+        for item in items:
+            total_cost += self.calculate_single_item_no_tax_nofees(item)
+        return total_cost
--- test_sample.py
+++ test_sample.py
@@ -7,3 +7,10 @@ def test_single_item_no_fees_no_taxes():
     actual = order.calculate_single_item_no_tax_nofees(items[0])
     assert expected == actual
     
+def test_multiple_items_no_fees_no_taxes():
+    items = [{"Name": "Nocco", "Price": 50, "Weight": 1, "Quantity": 1},
+             {"Name": "Collab", "Price": 40, "Weight": 1, "Quantity": 1}]
+    expected = 90
+    order = orders(1)
+    actual = order.calculate_multiple_items_no_tax_no_fees(items)
+    assert expected == actual
